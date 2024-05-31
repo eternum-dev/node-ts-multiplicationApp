c658422ef55bf2658a33f290bf58ca2921d2b4b6
eternum-dev <alejandro.thon.j@gmail.com>
2024-05-31 18:53:24 -0400

    add test

--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -3,6 +3,14 @@ import { SendFile } from "../domain/use-cases/send-file.use-case";
 import { ServerApp } from "./server-app";
 
 describe("Pruenas el server-app", () => {
+  const options = {
+    base: 4,
+    limit: 5,
+    showTable: true,
+    fileName: "test-fileName",
+    fileDestination: "test-fileDestination",
+  };
+
   test("should created serverApp instance", () => {
     const serverApp = new ServerApp();
 
@@ -11,32 +19,46 @@ describe("Pruenas el server-app", () => {
   });
 
   test("should run server with options ", () => {
-    const options = {
-      base: 4,
-      limit: 5,
-      showTable: true,
-      fileName: "test-fileName",
-      fileDestination: "test-fileDestination",
-    };
+    // const spyLog = jest.spyOn(console, "log");
+    // const spyCreateTable = jest.spyOn(CreateTable.prototype, "execute");
+    // const spySendFile = jest.spyOn(SendFile.prototype, "execute");
+    // ServerApp.run(options);
+    // expect(spyLog).toHaveBeenCalled();
+    // expect(spyLog).toHaveBeenCalledTimes(2);
+    // expect(spyLog).toHaveBeenCalledWith("Server Running...");
+    // expect(spyCreateTable).toHaveBeenCalledWith({
+    //   base: options.base,
+    //   limit: options.limit,
+    // });
+    // expect(spySendFile).toHaveBeenCalledWith({
+    //     fileContent: expect.any(String),
+    //     fileDestination: options.fileDestination,
+    //     fileName: options.fileName,
+    //   });
+  });
 
-    const spyLog = jest.spyOn(console, "log");
-    const spyCreateTable = jest.spyOn(CreateTable.prototype, "execute");
-    const spySendFile = jest.spyOn(SendFile.prototype, "execute");
+  test("should return with custom values mocked", () => {
+    const fileContent = "1x2=2";
+
+    const logMock = jest.fn();
+    const logErrorMock = jest.fn();
+    const createTableMock = jest.fn().mockReturnValue(fileContent);
+    const sendFileMock = jest.fn();
+
+    console.log = logMock;
+    console.error = logErrorMock;
+    CreateTable.prototype.execute = createTableMock;
+    SendFile.prototype.execute = sendFileMock;
 
     ServerApp.run(options);
 
-    expect(spyLog).toHaveBeenCalled();
-    expect(spyLog).toHaveBeenCalledTimes(2);
-    expect(spyLog).toHaveBeenCalledWith("Server Running...");
-
-    expect(spyCreateTable).toHaveBeenCalledWith({
-      base: options.base,
-      limit: options.limit,
+    expect(logMock).toHaveBeenCalledWith("Server Running...");
+    expect(logErrorMock).toHaveBeenCalledWith("algo error");
+    expect(createTableMock).toHaveBeenCalledWith({ base: 4, limit: 5 });
+    expect(sendFileMock).toHaveBeenCalledWith({
+      fileContent: "1x2=2",
+      fileDestination: options.fileDestination,
+      fileName: options.fileName,
     });
-    expect(spySendFile).toHaveBeenCalledWith({
-        fileContent: expect.any(String),
-        fileDestination: options.fileDestination,
-        fileName: options.fileName,
-      });
   });
 });
